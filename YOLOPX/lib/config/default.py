d45d1c83bc450d73aa17ce3caaa0e8ab1cbfb7d4
TysonTien0111 <TysonNguyen0111@gmail.com>
2026-05-16 23:17:26 -0700

    fix: Restore original BDD100K dataset paths for Colab training

--- YOLOPX/lib/config/default.py
+++ YOLOPX/lib/config/default.py
@@ -55,12 +55,12 @@ _C.LOSS.LL_IOU_GAIN = 0.2 # lane line iou loss gain
 
 # DATASET related params
 _C.DATASET = CN(new_allowed=True)
-_C.DATASET.DATAROOT = 'cloud_package/images'       # the path of images folder
-_C.DATASET.LABELROOT = 'cloud_package/labels'      # the path of det_annotations folder
-_C.DATASET.MASKROOT = 'cloud_package/masks'                # the path of da_seg_annotations folder
-_C.DATASET.LANEROOT = 'cloud_package/lanes'               # the path of ll_seg_annotations folder
+_C.DATASET.DATAROOT = 'bdd100k/bdd100k/images/100k'       # the path of images folder
+_C.DATASET.LABELROOT = 'data/bdd100k/labels'      # the path of det_annotations folder
+_C.DATASET.MASKROOT = 'data/bdd100k/bdd_seg_gt'                # the path of da_seg_annotations folder
+_C.DATASET.LANEROOT = 'data/bdd100k/bdd_lane_gt'               # the path of ll_seg_annotations folder
 _C.DATASET.DATASET = 'BddDataset'
-_C.DATASET.TRAIN_SET = 'val'
+_C.DATASET.TRAIN_SET = 'train'
 _C.DATASET.TEST_SET = 'val'
 _C.DATASET.DATA_FORMAT = 'jpg'
 _C.DATASET.SELECT_DATA = False
